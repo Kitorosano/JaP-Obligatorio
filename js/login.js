1d87ffaacd1310df0cac09f6ba595d79a8460fc3
Kitorosano <rosanoesteban2@gmail.com>
2021-09-17 01:45:08 -0300

    Entrega 3

--- js/login.js
+++ js/login.js
@@ -17,9 +17,3 @@ document.addEventListener("DOMContentLoaded", function(e){
   })
 
 });
-
-// function onSignIn(googleUser) {
-//   var profile = googleUser.getBasicProfile();
-//   localStorage.setItem('username',profile.getName());  
-//   location.replace('./home.html'); //Redireccione al home.html
-// }
