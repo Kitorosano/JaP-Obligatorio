193c89252cd2bc14059e6940a344a0edc350fff3
Kitorosano <rosanoesteban2@gmail.com>
2021-09-27 21:53:23 -0300

    Desafiante 4 Completado

--- js/product-info.js
+++ js/product-info.js
@@ -1,52 +1,56 @@
 let currentProduct = {};
 
-function showProductImages(array){
+function showProductImages(array) {
   let appendProductImage = "";
-  let imagesDivs = document.getElementById('productRelatedImages');
-  
-  for(let i = 1; i < array.length; i++){
-    let imageSrc = array[i];
-    
+  let appendProductImageIndicator = "";
+  let imagesDivs = document.getElementById('productImages');
+  let imagesIndicator = document.getElementById('productRelatedImages');
+
+	for (let i = 0; i < array.length; i++) {
+		let imageSrc = array[i];
+
+
     appendProductImage += `
-      <div class="col-md-3">
-        <div class="d-block mb-4 h-100">
-          <img class="img-thumbnail" src="${imageSrc}" alt="">
-        </div>
+      <div class="carousel-item ${i == 0 ? 'active' : ''}" class="d-block w-100">
+        <img class="img-thumbnail" src="${imageSrc}" alt="Slide ${i}">
       </div>`
-  }
     
+    appendProductImageIndicator += `
+      <li data-target="#carouselIndicators" data-slide-to="${i}" ${i == 0 ? 'class="active"' : ''}></li>`
+     
+	}
+    
+  imagesIndicator.innerHTML = appendProductImageIndicator;
   imagesDivs.innerHTML = appendProductImage;
 }
 
-function showProduct(){
-  let productNameHTML  = document.getElementById("productName");
-  let productMainImage = document.getElementById('productMainImage');
-  let productDescriptionHTML = document.getElementById("productDescription");
-  let productSoldCountHTML = document.getElementById("productSoldCount");
-  let productCriteriaHTML = document.getElementById("productCategory");
-  let productPriceHTML = document.getElementById("productPrice");
-  // let productsRelatedHTML = document.getElementById("relatedProducts");
-  
-  productNameHTML.innerHTML = currentProduct.name;
-  productMainImage.setAttribute('src',currentProduct.images[0]);
-  productDescriptionHTML.innerHTML = currentProduct.description;
-  productSoldCountHTML.innerHTML = currentProduct.soldCount;
-  productPriceHTML.innerHTML = `$${currentProduct.cost} ${currentProduct.currency}`
-  productCriteriaHTML.innerHTML = currentProduct.category;
-  // productsRelatedHTML.innerHTML = currentProduct.relatedProducts;
+function showProduct() {
+	let productNameHTML = document.getElementById('productName');
+	let productDescriptionHTML = document.getElementById('productDescription');
+	let productSoldCountHTML = document.getElementById('productSoldCount');
+	let productCriteriaHTML = document.getElementById('productCategory');
+	let productPriceHTML = document.getElementById('productPrice');
+	// let productsRelatedHTML = document.getElementById("relatedProducts");
 
-  //Muestro las imagenes en forma de galería
-  showProductImages(currentProduct.images); 
+	productNameHTML.innerHTML = currentProduct.name;
+	productDescriptionHTML.innerHTML = currentProduct.description;
+	productSoldCountHTML.innerHTML = currentProduct.soldCount;
+	productPriceHTML.innerHTML = `$${currentProduct.cost} ${currentProduct.currency}`;
+	productCriteriaHTML.innerHTML = currentProduct.category;
+	// productsRelatedHTML.innerHTML = currentProduct.relatedProducts;
+
+	//Muestro las imagenes en forma de galería
+	showProductImages(currentProduct.images);
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-  getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-    if (resultObj.status === "ok"){
-      currentProduct = resultObj.data;
-      showProduct();
-    }
-  });
+document.addEventListener('DOMContentLoaded', function (e) {
+	getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+		if (resultObj.status === 'ok') {
+			currentProduct = resultObj.data;
+			showProduct();
+		}
+	});
 });
